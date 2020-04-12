dd41a288bc6ffa5afa54bd425b75fa7562f575ab
Marat Dukhan <maratek@gmail.com>
2020-04-12 15:20:46 -0700

    Modernize CMake configuration files

--- cmake/DownloadGoogleBenchmark.cmake
+++ cmake/DownloadGoogleBenchmark.cmake
@@ -1,4 +1,4 @@
-CMAKE_MINIMUM_REQUIRED(VERSION 2.8.2)
+CMAKE_MINIMUM_REQUIRED(VERSION 3.5 FATAL_ERROR)
 
 PROJECT(googlebenchmark-download NONE)
 
@@ -6,8 +6,8 @@ INCLUDE(ExternalProject)
 ExternalProject_Add(googlebenchmark
 	URL https://github.com/google/benchmark/archive/v1.5.0.zip
 	URL_HASH SHA256=2d22dd3758afee43842bb504af1a8385cccb3ee1f164824e4837c1c1b04d92a0
-	SOURCE_DIR "${CONFU_DEPENDENCIES_SOURCE_DIR}/googlebenchmark"
-	BINARY_DIR "${CONFU_DEPENDENCIES_BINARY_DIR}/googlebenchmark"
+	SOURCE_DIR "${CMAKE_BINARY_DIR}/googlebenchmark-source"
+	BINARY_DIR "${CMAKE_BINARY_DIR}/googlebenchmark"
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ""
 	INSTALL_COMMAND ""
