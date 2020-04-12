6dff5c9d3db1aa355600eb8dfa1854357aed5005
Marat Dukhan <maratek@gmail.com>
2020-04-12 15:09:53 -0700

    Update Google Benchmark in CMake builds to 1.5.0

--- cmake/DownloadGoogleBenchmark.cmake
+++ cmake/DownloadGoogleBenchmark.cmake
@@ -4,8 +4,8 @@ PROJECT(googlebenchmark-download NONE)
 
 INCLUDE(ExternalProject)
 ExternalProject_Add(googlebenchmark
-	URL https://github.com/google/benchmark/archive/v1.2.0.zip
-	URL_HASH SHA256=cc463b28cb3701a35c0855fbcefb75b29068443f1952b64dd5f4f669272e95ea
+	URL https://github.com/google/benchmark/archive/v1.5.0.zip
+	URL_HASH SHA256=2d22dd3758afee43842bb504af1a8385cccb3ee1f164824e4837c1c1b04d92a0
 	SOURCE_DIR "${CONFU_DEPENDENCIES_SOURCE_DIR}/googlebenchmark"
 	BINARY_DIR "${CONFU_DEPENDENCIES_BINARY_DIR}/googlebenchmark"
 	CONFIGURE_COMMAND ""
